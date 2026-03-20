216187438c7fa422d712243cb8371b52e944aa64
Gitmazter <philip.andelic@gmail.com>
2026-03-20 16:48:37 +0700

    feat: add player and pathing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)]
 use macroquad::prelude::*;
+use std::collections::VecDeque;
 
 const MAP: usize = 20;
 const T_SIZE: (f32, f32) = (32., 16.);
@@ -10,23 +11,195 @@ enum AppState {
     GameOver
 }
 
+#[derive(Copy, Clone, PartialEq)]
+enum Tile {
+    Wall,
+    Floor,
+}
+
+//Math Helper
+fn to_screen(x: usize, y: usize, cam: (f32, f32)) -> (f32, f32) {
+    (
+        (x as f32 - y as f32) * T_SIZE.0 + cam.0,
+        (x as f32 + y as f32) * T_SIZE.1 + cam.1,
+    )
+}
+
+fn to_tile(sx: f32, sy: f32, cam: (f32, f32)) -> (usize, usize) {
+    let (ax, ay) = (sx - cam.0, sy - cam.1);
+    (
+        ((ax / T_SIZE.0 + ay / T_SIZE.1) / 2.) as usize,
+        ((ay / T_SIZE.1 - ax / T_SIZE.0) / 2.) as usize,
+    )
+}
+
+//Pathfinding algo
+fn bfs(
+    map: &[[Tile; MAP]; MAP],
+    start: (usize, usize),
+    goal: (usize, usize),
+) -> Vec<(usize, usize)> {
+    let mut q = VecDeque::from([start]);
+    let mut visited = [[false; MAP]; MAP];
+    visited[start.1][start.0] = true;
+
+    let mut parent = [[None; MAP]; MAP];
+
+    while let Some(curr) = q.pop_front() {
+        if curr == goal {
+            let mut path = vec![];
+            let mut c = goal;
+            while c != start {
+                path.push(c);
+                c = parent[c.1][c.0].unwrap();
+            }
+            path.reverse();
+            return path;
+        }
+
+        //check the close
+        for (dx, dy) in [(0, -1), (0, 1), (-1, 0), (1, 0)] {
+            let (nx, ny) = ((curr.0 as i32 + dx) as usize, (curr.1 as i32 + dy) as usize);
+
+            if nx < MAP && ny < MAP && !visited[ny][nx] && map[ny][nx] == Tile::Floor {
+                visited[ny][nx] = true;
+                parent[ny][nx] = Some(curr);
+                q.push_back((nx, ny));
+            }
+        }
+    }
+
+    vec![]
+}
+
+//draw hero and monsters
+fn draw_stickman(x: usize, y: usize, cam: (f32, f32)) {
+    let (sx, mut sy) = to_screen(x, y, cam);
+    sy += 16.;
+
+    // shadow
+    draw_ellipse(sx, sy+3., 10., 5., 0.,Color::new(0.,0.,0.,0.2));
+    //head
+    draw_circle_lines(sx, sy-32., 7., 2., BLACK);
+    //body and limbs
+    for l in [
+        [0.,-25.,0.,-8.],
+        [0.,-20.,-8.,-15.],
+        [0.,-20.,8.,-15.],
+        [0.,-8.,-6.,-0.],
+        [0.,-8.,6.,-0.]
+    ] {
+        draw_line(
+            sx + l[0], 
+            sy+l[1], 
+            sx+l[2], 
+            sy+l[3], 
+            2., 
+            BLACK
+        );
+    }
+}
+
+fn draw_wall(x: usize, y: usize, cam: (f32, f32)) {
+    let (sx, sy) = to_screen(x, y, cam);
+
+    let v = [
+        vec2(sx, sy - 40.),
+        vec2(sx + 32., sy - 24.),
+        vec2(sx, sy - 8.),
+        vec2(sx - 32., sy - 24.),
+        vec2(sx + 32., sy),
+        vec2(sx, sy + 16.),
+        vec2(sx - 32., sy),
+    ];
+
+    let colors = [
+        Color::new(0.8, 0.8, 0.8, 1.),
+        Color::new(0.5, 0.5, 0.5, 1.),
+        Color::new(0.6, 0.6, 0.6, 1.),
+    ];
+
+    //draw faces
+    draw_triangle(v[0], v[1], v[2], colors[0]);
+    draw_triangle(v[0], v[2], v[3], colors[0]);
+    draw_triangle(v[1], v[4], v[5], colors[1]);
+    draw_triangle(v[1], v[5], v[2], colors[1]);
+    draw_triangle(v[3], v[2], v[5], colors[2]);
+    draw_triangle(v[3], v[5], v[6], colors[2]);
+
+    //draw outline
+    for (a, b) in [(0, 1), (1, 2), (2, 3), (3, 0), (1, 4), (2, 5), (3, 6)] {
+        draw_line(v[a].x, v[a].y, v[b].x, v[b].y, 1., BLACK);
+    }
+}
+
 struct Game {
-    
+    map: [[Tile;MAP];MAP],
+    cam: (f32, f32),
+    px: usize,
+    py: usize,
+    path: Vec<(usize, usize)>
 }
 
 impl Game {
     fn new() -> Self {
-        Game {}
+        let mut map = [[Tile::Floor;MAP];MAP];
+        
+        for i in 0..MAP {
+            map[0][i] = Tile::Wall;
+            map[MAP-1][i] = Tile::Wall;
+            map[i][0] = Tile::Wall;
+            map[i][MAP-1] = Tile::Wall;
+        };
+
+        // obstacles
+        for (x,y) in [(5,5), (6,5), (12,10)] {
+            map[y][x] = Tile::Wall;
+        }
+
+        Game {
+            map,
+            cam: (screen_width() /2., 50.),
+            px: 2,
+            py: 2,
+            path: vec![]
+        }
     }
     fn update(&mut self, _dt:f32) -> bool {
         if is_key_pressed(KeyCode::Space) {
             return  true;
         }
+        if is_mouse_button_pressed(MouseButton::Left) {
+            let (mx, my) = mouse_position();
+            let (tx, ty) = to_tile(mx, my, self.cam);
+
+            //check if click in within bounds
+            if tx < MAP && ty < MAP && self.map[ty][tx] == Tile::Floor{
+                self.path = bfs(&self.map, (self.px, self.py), (tx, ty));
+            }
+        }
         false
     }
     fn draw(&self) {
-        draw_text("Game Running...", 20., 40., 30., BLACK);
-        draw_text("Press Space to DIE...", 20.,80.,20.,DARKGRAY);
+        for y in 0..MAP {
+            for x in 0..MAP {
+                if self.map[y][x] == Tile::Wall {
+                    draw_wall(x, y, self.cam);
+                } else {
+                    let (sx, sy) = to_screen(x, y, self.cam);
+                    draw_circle(sx, sy + 16., 2., LIGHTGRAY);
+                }
+            }
+
+            // draw the path
+            for (px, py) in &self.path {
+                let(sx,sy) = to_screen(*px, *py, self.cam);
+                draw_circle(sx, sy+16., 4., GOLD);
+            }
+
+            //draw the player
+            draw_stickman(self.px, self.py, self.cam);
+        }
     }
 }
 
